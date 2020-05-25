57b421d6a6d34ad489afeb182fae95efb29d959d
mathur15 <som.mathur1997@gmail.com>
2020-05-25 08:22:48 -0400

    Initial Setup-Dataset and packages

---
+++ cnn.py
@@ -0,0 +1,7 @@
+#structuring the folder in terms of the outputs, to single out the 
+#output classses for keras
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
