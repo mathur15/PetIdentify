3b0d3a62930391b05091acb1e1df6794956e044c
mathur15 <som.mathur1997@gmail.com>
2020-05-29 11:29:21 -0400

    Image augmentation and model is fit

--- cnn.py
+++ cnn.py
@@ -1,10 +1,11 @@
-#structuring the folder in terms of the outputs, to single out the 
+ #structuring the folder in terms of the outputs, to single out the 
 #output classses for keras
 from keras.models import Sequential
 from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+from keras.preprocessing.image import ImageDataGenerator
 
 #initialize CNN
 classifier = Sequential()
@@ -28,6 +29,38 @@ classifier.add(Flatten())
 classifier.add(Dense(output_dim = 128,activation='relu'))
 classifier.add(Dense(output_dim = 1,activation = 'sigmoid'))
 
-classifier.compile(optimizer='adam', loss='binary_crossentropy',metrics=['accuracy'])
+classifier.compile(optimizer='adam', loss='binary_crossentropy',
+                   metrics=['accuracy'])
 
-#Fit the images
+#Fit the images-image augmentation to prevent overfitting- Check 
+#General Notes file for more info
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+#class_mode is binary as there are two possible outcomes
+train_set = test_datagen.flow_from_directory(
+           'training_set',
+            target_size=(64, 64),
+            batch_size=32,
+            class_mode='binary')
+
+test_set = test_datagen.flow_from_directory(
+           'test_set',
+           target_size=(64, 64),
+           batch_size=32,
+           class_mode='binary')
+
+#fit on train set and test performance on the test set
+classifier.fit(
+           train_set,
+           steps_per_epoch=8000,
+           epochs=25,
+           validation_data=test_set,
+           validation_steps=2000)
+
+
