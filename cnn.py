6eda99f874948804313feda0432bb76ccc416ccc
mathur15 <som.mathur1997@gmail.com>
2020-06-02 20:57:10 -0400

    ~79 accuracy now trying more layers

--- cnn.py
+++ cnn.py
@@ -8,6 +8,7 @@ from keras.layers import Dense
 from keras.layers import Dropout
 from keras.preprocessing.image import ImageDataGenerator
 from keras.preprocessing import image
+import numpy as np
 
 #initialize CNN
 classifier = Sequential()
@@ -18,6 +19,9 @@ classifier.add(Convolution2D(32,3,3,input_shape=(64,64,3),activation='relu'))
 classifier.add(MaxPooling2D(pool_size=(2,2)))
 
 #to optimize the accuracy on the test add another convolution layer
+classifier.add(Convolution2D(32,3,3,activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2,2)))
+
 classifier.add(Convolution2D(32,3,3,activation='relu'))
 classifier.add(MaxPooling2D(pool_size=(2,2)))
 
@@ -36,8 +40,9 @@ classifier.add(Flatten())
 #     print(layers.name,filter.shape)
 # =============================================================================
 #Good practice- take powers of 2 for hidden layers
-classifier.add(Dense(output_dim = 128,activation='relu'))
-#classifier.add(Dropout(0.1))
+classifier.add(Dense(output_dim = 64,activation='relu'))
+classifier.add(Dropout(0.4))
+classifier.add(Dense(output_dim = 64,activation='relu'))
 classifier.add(Dense(output_dim = 1,activation = 'sigmoid'))
 
 classifier.compile(optimizer='adam', loss='binary_crossentropy',
@@ -76,11 +81,13 @@ classifier.fit(
 
 #make a single prediction
 
-test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', 
-                            target_size(64,64))
+test_image = image.load_img('single_prediction/cat_or_dog_1.jpg', 
+                            target_size=(64,64))
 test_image = image.img_to_array(test_image)
+#To make it compatible and make a batch of one input
 test_image = np.expand_dims(test_image,axis=0)
 result = classifier.predict(test_image)
-print(training_set.class_indices)
+#to find out if the output corresponds to cat or dog
+print(result.class_indices)
 
 
